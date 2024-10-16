bc04dcb798f7cdd3bdb50866854464f85b46a5b4
Aurélien <aurelien.strimelle@bstorm.be>
2024-10-16 14:05:31 +0200

    Initialize project 🦄

---
+++ README.md
@@ -0,0 +1,4 @@
+# Demo requete
+
+## Librairie
+- Axios
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Demo requete Ajax</h1>
+      
+    </>
+  )
+}
+
+export default App
